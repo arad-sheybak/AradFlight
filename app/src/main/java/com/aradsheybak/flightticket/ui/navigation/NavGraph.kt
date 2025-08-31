49abff5138eb05f034d9d26fd8b03ba2862b38c6
Arad <arad.sheybak@gmail.com>
2025-08-31 08:29:48 +0330

    splash screen completed

--- app/src/main/java/com/aradsheybak/flightticket/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/aradsheybak/flightticket/ui/navigation/NavGraph.kt
@@ -5,6 +5,8 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.aradsheybak.flightticket.ui.screens.HomeScreen
+import com.aradsheybak.flightticket.ui.screens.SplashScreen
 import kotlinx.coroutines.delay
 
 @Composable
@@ -12,17 +14,13 @@ fun AppNavHost(navController: NavHostController) {
     NavHost(navController = navController, startDestination = Screen.Splash.route) {
 
         composable(Screen.Splash.route) {
-            LaunchedEffect(Unit) {
-                delay(3000)
-                navController.navigate(Screen.Home.route) {
-                    popUpTo(Screen.Splash.route) { inclusive = true }
-                }
-            }
+            SplashScreen(navController)
+
         }
 
-//        composable(Screen.Home.route) {
-//            HomeScreen()
-//        }
+        composable(Screen.Home.route) {
+            HomeScreen()
+        }
 //
 //        composable(Screen.Login.route) {
 //            LoginScreen()
--- app/src/main/java/com/aradsheybak/flightticket/ui/theme/Color.kt
+++ app/src/main/java/com/aradsheybak/flightticket/ui/theme/Color.kt
@@ -9,3 +9,7 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PurpleBase = Color(0XFF2C1C4B)
+
+val DarkPurpleBase = Color(0XFF1E1E20)
